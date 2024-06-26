91c2fe1d671c8027505960a95e23457c72e32a4f
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2024-06-26 14:13:26 +0530

    initial

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Amlan", age: 19, email: "amlan@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
